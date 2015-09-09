f741c48e35568dad4f912bc2753070f3fbed360e
Sam Tsai <sam.iagd@hotmail.com>
2015-09-10 05:31:52 +0800

    rewrite exports

--- www/wechat.js
+++ www/wechat.js
@@ -1,65 +1,86 @@
 ﻿/*
-    Cordova WeChat Plugin
-    https://github.com/vilic/cordova-plugin-wechat
+ Cordova WeChat Plugin
+ https://github.com/vilic/cordova-plugin-wechat
 
-    by VILIC VANE
-    https://github.com/vilic
+ by VILIC VANE
+ https://github.com/vilic
 
-    MIT License
-*/
+ MIT License
+ */
 
-exports.share = function (message, scene, onfulfill, onreject) {
-    var ThenFail = window.ThenFail;
-    var promise;
+//exports.share = function (message, scene, onfulfill, onreject) {
+//    var ThenFail = window.ThenFail;
+//    var promise;
+//
+//    if (ThenFail && !onfulfill && !onreject) {
+//        promise = new ThenFail();
+//    }
+//
+//    var text = null;
+//
+//    if (typeof message == 'string') {
+//        text = message;
+//        message = null;
+//    }
+//
+//    cordova
+//        .exec(function () {
+//            if (promise) {
+//                promise.resolve();
+//            } else if (onfulfill) {
+//                onfulfill();
+//            }
+//        }, function (err) {
+//            if (promise) {
+//                promise.reject(err);
+//            } else if (onreject) {
+//                onreject(err);
+//            }
+//        }, 'WeChat', 'share', [
+//            {
+//                message: message,
+//                text: text,
+//                scene: scene
+//            }
+//        ]);
+//
+//    return promise;
+//};
 
-    if (ThenFail && !onfulfill && !onreject) {
-        promise = new ThenFail();
-    }
+var exec = require('cordova/exec'),
+    cordova = require('cordova');
 
-    var text = null;
+module.exports = {
+    share: function (message, scene, sucessCallBack, errorCallBack) {
+        var text = null;
+        if (typeof message == 'string') {
+            text = message;
+            message = null;
+        }
 
-    if (typeof message == 'string') {
-        text = message;
-        message = null;
-    }
-
-    cordova
-        .exec(function () {
-            if (promise) {
-                promise.resolve();
-            } else if (onfulfill) {
-                onfulfill();
-            }
-        }, function (err) {
-            if (promise) {
-                promise.reject(err);
-            } else if (onreject) {
-                onreject(err);
-            }
-        }, 'WeChat', 'share', [
+        exec(sucessCallBack, errorCallBack, 'WeChat', 'share', [
             {
                 message: message,
                 text: text,
                 scene: scene
             }
         ]);
+    },
 
-    return promise;
+    Scene: {
+        chosenByUser: 0,
+        session: 1,
+        timeline: 2
+    },
+
+    ShareType: {
+        app: 1,
+        emotion: 2,
+        file: 3,
+        image: 4,
+        music: 5,
+        video: 6,
+        webpage: 7
+    }
 };
 
-
-exports.Scene = {
-    chosenByUser: 0,
-    session: 1,
-    timeline: 2
-};
-
-exports.ShareType = {
-    app: 1,
-    emotion: 2,
-    file: 3,
-    image: 4,
-    music: 5,
-    video: 6,
-    webpage: 7
-};
